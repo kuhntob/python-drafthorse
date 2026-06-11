3dfda5ea7836090408fd0fa4ff3bc528c23b6435
Tobias Kuhn <tobias.kuhn.73@googlemail.com>
2026-06-11 13:11:25 +0200

    update schema to 1.09

--- drafthorse/utils.py
+++ drafthorse/utils.py
@@ -12,7 +12,7 @@ def validate_xml(xmlout, schema):
         return xmlout
     if schema is not None:
         if schema.startswith("FACTUR-X_"):
-            schema = schema.replace("FACTUR-X_", "Factur-X_1.0.07_")
+            schema = schema.replace("FACTUR-X_", "Factur-X_1.09_")
             schema = schema.replace("BASIC-WL", "BASICWL")
         schema = etree.XMLSchema(
             file=os.path.join(os.path.dirname(__file__), "schema", schema + ".xsd")
